ba7d9c61f53207a45e83446aa8337d36e88cfa53
Andrew Parks and Charlie Kelson <charlie@>
2017-08-02 11:02:51 -0700

    this commit will add new functionality

--- Models/Task.cs
+++ Models/Task.cs
@@ -2,35 +2,43 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-    public class Task
+  public class Task
+  {
+    private string _description;
+    private int _id;
+    private static List<Task> _instances = new List<Task> {};
+
+    public Task(string description)
     {
-        private string _description;
-        private static List<string> _instances = new List<string> {};
-
-        public Task (string description)
-        {
-            _description = description;
-        }
-        public string GetDescription()
-        {
-            return _description;
-        }
-        public void SetDescription(string newDescription)
-        {
-            _description = newDescription;
-        }
-        public static List<string> GetAll()
-        {
-            return _instances;
-        }
-        public void Save()
-        {
-            _instances.Add(_description);
-        }
-
-        public static void ClearAll()
-        {
-            _instances.Clear();
-        }
+      _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
     }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Task> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Task Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
 }
